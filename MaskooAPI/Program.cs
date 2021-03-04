275ad83a8dcbd628f25ea627c0a046b7a32ae3c6
Max MARIE <max.marie@finalcad.com>
2021-03-04 16:20:50 +0100

    change kestrel url

--- MaskooAPI/Program.cs
+++ MaskooAPI/Program.cs
@@ -20,7 +20,7 @@ namespace MaskooAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http:localhost:7000");
                 });
     }
 }
