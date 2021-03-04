b96d9ab1c5ea33d97e9438b52a333ee9fc1b7c88
Max MARIE <max.marie@finalcad.com>
2021-03-04 16:30:10 +0100

    expose swagger

--- MaskooAPI/Startup.cs
+++ MaskooAPI/Startup.cs
@@ -105,12 +105,9 @@ namespace MaskooAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MaskooAPI v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MaskooAPI v1"));
 
             // global cors policy
             app.UseCors(x => x
