b8df17bc7ed307b0d1a2004ced67e8213d9767cb
mmarie4 <max.marie.4@gmail.com>
2021-05-27 19:38:19 +0200

    add id to tool dto

--- MaskooAPI/Models/Toolboxes/ToolResponse.cs
+++ MaskooAPI/Models/Toolboxes/ToolResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace MaskooAPI.Models.Toolboxes
+﻿using System;
+
+namespace MaskooAPI.Models.Toolboxes
 {
     public class ToolResponse
     {
+        public Guid Id { get; set; }
+
         public string Label { get; set; }
 
         public string Value { get; set; }
